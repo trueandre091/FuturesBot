d25887f557d58d77ad68a09a52d2ca8d166ac06b
andre <151407275+trueandre091@users.noreply.github.com>
2024-10-20 01:19:40 +0200

    fixes

--- fun/turn.py
+++ fun/turn.py
@@ -1,10 +1,9 @@
-with open("markers_long.txt") as file:
-    markers_long = [i.strip("\n") for i in file]
-with open("markers_short.txt") as file:
-    markers_short = [i.strip("\n") for i in file]
-
-
 def get_turn_markers(message):
+    with open("markers_long.txt") as file:
+        markers_long = [i.strip("\n") for i in file]
+    with open("markers_short.txt") as file:
+        markers_short = [i.strip("\n") for i in file]
+
     words = [i.lower() for i in message.split()]
     for marker in markers_long:
         if marker in words:
